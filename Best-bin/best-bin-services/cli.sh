c135f3c762b26a14a2783e6f538ee5b83c47a609
Mohan Ramu <mohan.ramu@sap.com>
2020-03-26 20:04:11 +0530

    CLI updated.
    Change-Id: Ie8c2bb0ba45bffd3150ba152138045a7d665284b

--- Best-bin/best-bin-services/cli.sh
+++ Best-bin/best-bin-services/cli.sh
@@ -5,22 +5,28 @@ set -e
 APP_NAME="best-service"
 APP_VERSION="v1"
 
-echo "Clean Install and Package ====";
+echo "Clean Install and Package..";
 mvn clean install -DskipTests
 
 mvn clean package -DskipTests
+echo "Clean Install and Package Done.";
 
-echo "Enable Google Container Registry  ====";
+echo "Enable Google Container Registry..";
 gcloud services enable containerregistry.googleapis.com
+echo "Container Registry Enabled.";  
   
-echo "Create the container image and push it to the Container Registry  ====";  
+echo "Create the container image and push it to the Container Registry..";  
 mvn com.google.cloud.tools:jib-maven-plugin:build \
 -Dimage=gcr.io/$GOOGLE_CLOUD_PROJECT/$APP_NAME:$APP_VERSION
+echo "Container Created.";  
   
-echo "Re Deploy  ====";    
+echo "Deployment started..";    
 kubectl delete deployment best-service
 
 kubectl create deployment $APP_NAME \
 --image=gcr.io/$GOOGLE_CLOUD_PROJECT/$APP_NAME:$APP_VERSION
-  
+echo "Deployment Done.";   
+echo "Create Service Load-balancer..";  
+kubectl delete service  $APP_NAME
 kubectl create service loadbalancer $APP_NAME --tcp=8080:8080
+echo "Create Service Load-balancer Done. ";  
