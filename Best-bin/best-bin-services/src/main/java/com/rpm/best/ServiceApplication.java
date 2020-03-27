a0e30a347356ff671ae06492791788f21944377e
Mohan Ramu <mohan.ramu@sap.com>
2020-03-27 17:08:12 +0530

    improved scanning
    Change-Id: I3d3d40481ea7c4d09890c39a757d8d572ee134d6

--- Best-bin/best-bin-services/src/main/java/com/rpm/best/ServiceApplication.java
+++ Best-bin/best-bin-services/src/main/java/com/rpm/best/ServiceApplication.java
@@ -2,9 +2,11 @@ package com.rpm.best;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.rpm.best"})
 public class ServiceApplication {
 
 	public static void main(String[] args) {
