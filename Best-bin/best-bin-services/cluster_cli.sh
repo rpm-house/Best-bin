12b1fb40d45b7ac56282d47dce8f5cff117b5a88
Mohan Ramu <mohan.ramu@sap.com>
2020-03-27 19:26:31 +0530

    cli fro cluster created
    Change-Id: I003a4a6d928b8de31260c07d752918c602c577d7

---
+++ Best-bin/best-bin-services/cluster_cli.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# name of your app 
+CLUSTER_NAME="best-service-cluster"
+
+echo "====================================================="; 
+echo "Enable Compute Service..";
+gcloud services enable compute.googleapis.com container.googleapis.com
+echo "Create Cluster..";
+gcloud container clusters create $CLUSTER_NAME \
+  --num-nodes 2 \
+  --machine-type n1-standard-1 \
+  --zone us-central1-c
+echo "====================================================="; 
